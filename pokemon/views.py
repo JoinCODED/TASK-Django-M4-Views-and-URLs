e524c915d4a9efbef58062f2a270def0dbc1ae2d
Abdullah <adbdb.27@gmail.com>
2022-07-19 19:58:50 +0300

    done

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,11 @@
 from django.http import HttpResponse
 
 from .models import Pokemon
+def get_pokemon(request,pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return HttpResponse(f"<p>Pokemon name: {pokemon.name}</p>  <p>Pokemon Type: {pokemon.type}</p>  <p>Pokemon power: {pokemon.hp}</p> ")
+
+def get_pokemons(request):
+    pokemons = Pokemon.objects.all()
+    pokemons_list = [f"<li><a href =/pokemons/{pokemon.id}> {pokemon.name}</li></a>" for pokemon in pokemons]
+    return HttpResponse(pokemons_list)
